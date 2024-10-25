66eb7225bdf760a270a3b70df413cc25d06f223b
NoaT <nteomi@gmail.com>
2024-10-25 18:57:05 +0300

    Exclude media and static serving lines from coverage in urls.py

--- Yoga/Yoga/urls.py
+++ Yoga/Yoga/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
 
 # Serve media files during development
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # pragma: no cover
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # pragma: no cover
